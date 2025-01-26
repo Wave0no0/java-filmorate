72b50f835aa147d39a1d38b53bed82e1041ca3c6
wave <vlad3056968@yandex.ru>
2025-01-26 17:59:35 +0300

    fix base

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,44 +1,17 @@
--- Удаляем все данные для чистого старта
-DELETE FROM likes;
-DELETE FROM film_genres;
-DELETE FROM films;
-DELETE FROM genres;
-DELETE FROM ratings;
+UPDATE genres SET name = 'Мультфильм' WHERE id = 3;
+UPDATE genres SET name = 'Триллер' WHERE id = 4;
+UPDATE genres SET name = 'Документальный' WHERE id = 5;
+UPDATE genres SET name = 'Боевик' WHERE id = 6;
 
--- Добавляем данные в таблицу жанров
-INSERT INTO genres (id, name) VALUES
-(1, 'Комедия'),
-(2, 'Драма'),
-(3, 'Мультфильм'),
-(4, 'Триллер'),
-(5, 'Документальный'),
-(6, 'Боевик');
+MERGE INTO genres (id, name) KEY(id) VALUES (1, 'Комедия');
+MERGE INTO genres (id, name) KEY(id) VALUES (2, 'Драма');
+MERGE INTO genres (id, name) KEY(id) VALUES (3, 'Мультфильм');
+MERGE INTO genres (id, name) KEY(id) VALUES (4, 'Триллер');
+MERGE INTO genres (id, name) KEY(id) VALUES (5, 'Документальный');
+MERGE INTO genres (id, name) KEY(id) VALUES (6, 'Боевик');
 
--- Добавляем данные в таблицу рейтингов
-INSERT INTO ratings (id, name) VALUES
-(1, 'G'),
-(2, 'PG'),
-(3, 'PG-13'),
-(4, 'R'),
-(5, 'NC-17');
-
--- Добавляем данные в таблицу фильмов
-INSERT INTO films (id, name, description, release_date, duration, mpa_rating) VALUES
-(1, 'Интерстеллар', 'Фантастический фильм о космосе.', '2014-11-07', 169, 3),
-(2, 'Начало', 'Фильм о мире снов.', '2010-07-16', 148, 4),
-(3, 'Король Лев', 'Анимационный фильм о приключениях льва.', '1994-06-15', 88, 1);
-
--- Добавляем связи фильмов с жанрами
-INSERT INTO film_genres (film_id, genre_id) VALUES
-(1, 1), -- Интерстеллар - Комедия
-(1, 2), -- Интерстеллар - Драма
-(2, 4), -- Начало - Триллер
-(3, 3), -- Король Лев - Мультфильм
-(3, 1); -- Король Лев - Комедия
-
--- Добавляем лайки к фильмам
-INSERT INTO likes (film_id, user_id) VALUES
-(1, 1), -- Лайк Интерстеллару от пользователя 1
-(1, 2), -- Лайк Интерстеллару от пользователя 2
-(2, 3), -- Лайк Началу от пользователя 3
-(3, 1); -- Лайк Королю Льву от пользователя 1
+MERGE INTO ratings (id, name) KEY(id) VALUES (1, 'G');
+MERGE INTO ratings (id, name) KEY(id) VALUES (2, 'PG');
+MERGE INTO ratings (id, name) KEY(id) VALUES (3, 'PG-13');
+MERGE INTO ratings (id, name) KEY(id) VALUES (4, 'R');
+MERGE INTO ratings (id, name) KEY(id) VALUES (5, 'NC-17');
